07b6bc17fe36db1f7d287cd19c8672788e6c742b
Sergio Ortiz <sergio.ortizflores@gmail.com>
2019-08-22 11:20:11 +0800

    Initial Commit

---
+++ someApp/SomeAppViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  someApp
+//
+//  Created by Sergio Ortiz on 22.08.19.
+//  Copyright © 2019 sergioortiz.com. All rights reserved.
+//
+
+import UIKit
+
+class SomeAppViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
