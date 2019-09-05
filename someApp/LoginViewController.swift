26fd75781acd74773dc44e33da69b6b66619ca66
Sergio Ortiz <sergio.ortizflores@gmail.com>
2019-09-05 12:02:25 +0800

    login scene OK.  Before setting up the DB.

---
+++ someApp/LoginViewController.swift
@@ -0,0 +1,86 @@
+//
+//  LoginViewController.swift
+//  someApp
+//
+//  Created by Sergio Ortiz on 05.09.19.
+//  Copyright © 2019 sergioortiz.com. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var textFieldLoginEmail: UITextField!
+    @IBOutlet weak var textFieldLoginPassword: UITextField!
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "loginOK", sender: nil)
+    }
+    
+    
+    @IBAction func signUpPressed(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Register",
+                                      message: "Register",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel)
+        
+        alert.addTextField { textEmail in
+            textEmail.placeholder = "Enter your email"
+        }
+        
+        alert.addTextField { textPassword in
+            textPassword.isSecureTextEntry = true
+            textPassword.placeholder = "Enter your password"
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == textFieldLoginEmail {
+            textFieldLoginPassword.becomeFirstResponder()
+        }
+        if textField == textFieldLoginPassword {
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+}
